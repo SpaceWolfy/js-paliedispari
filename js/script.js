5bd8383e948e5e322ef6f0f49ba224e71968ebf5
SpaceWolfy <s_scozzari@libero.it>
2021-12-16 16:16:14 +0100

    Snack 2 completed

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ console.log(verifica);
 
 //Snack 2
 //L'utente sceglie se tirare pari o dispari
-let pariDispari = prompt('Scegli pari o dispari');
+/* let pariDispari = prompt('Scegli pari o dispari');
 
 let numeroScelto;
 //eseguo un controllo sulla risposta dell'utente
@@ -67,3 +67,49 @@ if (pariDispari == 'dispari' && machine == 1) {
 }
 console.log(machine);
 console.log(pariDispari);
+ */
+
+// Riscrittura Snack 2
+let pariDispari = prompt('Scegli pari o dispari');
+let numeroScelto;
+//eseguo un controllo sulla risposta dell'utente
+function numeroPlayer1 (numeroScelto) {
+
+    if (pariDispari !== 'pari' && pariDispari !== 'dispari') {
+        alert('Ti avevo detto di inserire pari o dispari!');
+
+    } else {
+        numeroScelto = parseInt(prompt('Ora scegli un numero tra 1 e 5'));
+        if (numeroScelto > 5 || numeroScelto < 1) {
+            alert('Hai rovinato il gioco -_-');
+        }
+    }
+
+    return numeroScelto;
+}
+let player1Num = numeroPlayer1(numeroScelto);
+console.log(player1Num);
+
+//Creo una funzione che generi un numero random per la macchina
+
+function numeroMacchina (maxNum, minNum) {
+    result = Math.floor(Math.random() * ((maxNum + 1)- minNum)) + minNum;
+
+    return result
+};
+let machineNum = numeroMacchina(1, 5);
+console.log(machineNum);
+
+const sum = player1Num + machineNum;
+console.log(sum);
+
+function winSituation (sum) {
+    if (sum % 2 === 0) {
+        console.log ('Ha vinto pari');
+    } else {
+        console.log('Ha vinto dispari');
+    }
+    return sum;
+}
+
+let win = winSituation (sum);
