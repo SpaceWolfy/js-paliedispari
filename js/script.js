967caa3c81f13f4220bc88830c5eaef0226826f6
SpaceWolfy <s_scozzari@libero.it>
2021-12-16 14:06:41 +0100

    snack 1 completed - palindrome speech

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//chiedere di inserire una parola:
+let answer = prompt('Inserisci una parola! Magari palindroma...')
+let palindroma = false;
+
+//La funzione verifica che la parola sia palindroma
+function verificaPal(answer, palindroma) {
+    if (answer === answer.split('').reverse().join('')) {
+        palindroma = true;
+    }
+    console.log(palindroma);
+    //implica il ritorno dell'elemento contenuto nella variabile answer, ma letto al contrario
+    return answer.split('').reverse().join('');
+}
+
+let verifica = verificaPal(answer, palindroma);
+
