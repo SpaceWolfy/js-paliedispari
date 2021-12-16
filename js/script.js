dc6792718fb79b588c369943c4de4b08282cc286
SpaceWolfy <s_scozzari@libero.it>
2021-12-16 14:45:54 +0100

    added additional controls for the user

--- js/script.js
+++ js/script.js
@@ -22,5 +22,17 @@ let pariDispari = prompt('Scegli pari o dispari');
 //eseguo un controllo sulla risposta dell'utente
 if (pariDispari !== 'pari' && pariDispari !== 'dispari') {
     alert('Ti avevo detto di inserire pari o dispari!');
+} else if (pariDispari === 'pari') {
+    let numeroPari = parseInt(prompt('Ora scegli un numero tra 2 e 4'));
+    if(numeroPari !== 2 && numeroPari !== 4){
+        alert('Ti avevo detto di inserire 2 o 4!');
+    }
+    console.log(numeroPari)
+} else if (pariDispari === 'dispari') {
+    let numeroDisp = parseInt(prompt('Ora scegli un numero tra 1, 3 e 5'));
+    if(numeroDisp !== 1 && numeroDisp !== 3 && numeroDisp !== 5){
+        alert('Ti avevo detto di inserire 1, 3 o 5!');
+    }
+    console.log(numeroDisp)
 }
 console.log(pariDispari);
