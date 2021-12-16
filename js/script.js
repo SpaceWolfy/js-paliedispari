76a1d7291630c369c8983d2aba45b1cc930cf591
SpaceWolfy <s_scozzari@libero.it>
2021-12-16 15:08:56 +0100

    inserted in a function the controls for the user

--- js/script.js
+++ js/script.js
@@ -18,21 +18,32 @@ console.log(verifica);
 
 //Snack 2
 //L'utente sceglie se tirare pari o dispari
+let machine = 0;
 let pariDispari = prompt('Scegli pari o dispari');
+
+let numeroScelto;
 //eseguo un controllo sulla risposta dell'utente
-if (pariDispari !== 'pari' && pariDispari !== 'dispari') {
-    alert('Ti avevo detto di inserire pari o dispari!');
-} else if (pariDispari === 'pari') {
-    let numeroPari = parseInt(prompt('Ora scegli un numero tra 2 e 4'));
-    if(numeroPari !== 2 && numeroPari !== 4){
-        alert('Ti avevo detto di inserire 2 o 4!');
+function numeroPlayer1 (numeroScelto) {
+
+    if (pariDispari !== 'pari' && pariDispari !== 'dispari') {
+        alert('Ti avevo detto di inserire pari o dispari!');
+
+    } else if (pariDispari === 'pari') {
+        numeroScelto = parseInt(prompt('Ora scegli un numero tra 2 e 4'));
+        if(numeroScelto !== 2 && numeroScelto !== 4){
+            alert('Ti avevo detto di inserire 2 o 4!');
+        };
+    } else if (pariDispari === 'dispari') {
+        numeroScelto = parseInt(prompt('Ora scegli un numero tra 1, 3 e 5'));
+        if(numeroScelto !== 1 && numeroScelto !== 3 && numeroScelto !== 5){
+            alert('Ti avevo detto di inserire 1, 3 o 5!');
+        };
     }
-    console.log(numeroPari)
-} else if (pariDispari === 'dispari') {
-    let numeroDisp = parseInt(prompt('Ora scegli un numero tra 1, 3 e 5'));
-    if(numeroDisp !== 1 && numeroDisp !== 3 && numeroDisp !== 5){
-        alert('Ti avevo detto di inserire 1, 3 o 5!');
-    }
-    console.log(numeroDisp)
+
+return numeroScelto;
 }
-console.log(pariDispari);
+
+let player1 = numeroPlayer1(numeroScelto);
+console.log(player1);
+
+//Creo una funzione che generi un numero random per la macchina
